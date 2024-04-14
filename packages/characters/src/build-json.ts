2c7b3a4fe01ecc101fff61e46ef1c3f7da696002
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-04-14 21:44:41 +0800

    refactor(scripts): try mkdir

--- packages/characters/src/build-json.ts
+++ packages/characters/src/build-json.ts
@@ -9,10 +9,14 @@ try {
 } catch {}
 
 for (const [category, characters] of Object.entries(json)) {
-  await fs.mkdir(path.join('./dist', category))
+  try {
+    await fs.mkdir(path.join('./dist', category))
+  } catch {}
 
   for (const [character, variants] of Object.entries(characters)) {
-    await fs.mkdir(path.join('./dist', category, character))
+    try {
+      await fs.mkdir(path.join('./dist', category, character))
+    } catch {}
 
     for (const [variant, json] of Object.entries(variants)) {
       await fs.writeFile(path.join('./dist', category, character, `${variant}.json`), stringifyCharacterCard(json))
