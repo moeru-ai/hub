b7914539eabe1e7f874a631bfea50f73b9f8a4e0
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-04-15 23:26:38 +0800

    chore(characters): fix utils

--- packages/characters/src/utils.ts
+++ packages/characters/src/utils.ts
@@ -1,15 +1,12 @@
-import { defineCharacterCard as origianlDefineCharacterCard, overrideCharacterCard as originalOverrideCharacterCard, type CharacterCardMerger, type CharacterCardData, type CharacterCard } from '@moeru-ai/character-card-helpers'
+import { defineCharacterCard as originalDefineCharacterCard, type CharacterCardMerger, type CharacterCardData, type CharacterCard } from '@moeru-ai/character-card-helpers'
 import defu from 'defu'
 
 const merge: CharacterCardMerger = (overrides, defaults) => defu(overrides, defaults)
 
-export const defineCharacterCard = (data: Partial<CharacterCardData>) => origianlDefineCharacterCard(Object.fromEntries(
-  new Set(Object.entries({
-    creator: 'moeru-ai',
-    character_version: '0.1',
-    ...data,
-  })
-    .sort(([keyA], [keyB]) => keyA.localeCompare(keyB)))
-), merge)
+export const defineCharacterCard = (data: Partial<CharacterCardData>) => originalDefineCharacterCard({
+  creator: 'moeru-ai',
+  character_version: '0.1',
+  ...data,
+}, merge)
 
-export const overrideCharacterCard = (overrides: Partial<CharacterCardData>, defaults: CharacterCard) => originalOverrideCharacterCard(overrides, defaults, merge)
+export const overrideCharacterCard = (overrides: Partial<CharacterCardData>, defaults: CharacterCard) => defineCharacterCard(merge(overrides, defaults.data))
