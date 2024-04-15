e93fdab9a8143793fa47f81a59d64d12c97c7fa7
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-04-15 22:27:31 +0800

    fix(docs): fix edit link pattern

--- docs/.vitepress/config/edit-link.ts
+++ docs/.vitepress/config/edit-link.ts
@@ -4,7 +4,7 @@ export const editLink = {
     // pattern: 'https://github.com/moeru-ai/hub/edit/main/:path'
     pattern: ({ filePath }) => {
         if (filePath.startsWith('characters/'))
-            return `https://github.com/moeru-ai/hub/edit/main/packages/characters/src/json/${filePath.slice(11, -3)}.ts`
+            return `https://github.com/moeru-ai/hub/edit/main/packages/characters/src/${filePath.slice(11, -3)}.ts`
         else return `https://github.com/moeru-ai/hub/edit/main/docs/${filePath}`
     }
 } satisfies Theme.EditLink
