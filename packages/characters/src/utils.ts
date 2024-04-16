39013b52e8bddd7e33b3ad9a96be79f6843b13c7
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-04-16 10:13:51 +0800

    refactor(characters): improve structure

--- packages/characters/src/utils.ts
+++ packages/characters/src/utils.ts
@@ -1,12 +1,10 @@
-import { defineCharacterCard as originalDefineCharacterCard, type CharacterCardMerger, type CharacterCardData, type CharacterCard } from '@moeru-ai/character-card-helpers'
+import { defineCharacterCard as originalDefineCharacterCard, type CharacterCardData, type CharacterCard } from '@moeru-ai/character-card-helpers'
 import defu from 'defu'
-
-const merge: CharacterCardMerger = (overrides, defaults) => defu(overrides, defaults)
 
 export const defineCharacterCard = (data: Partial<CharacterCardData>) => originalDefineCharacterCard({
   creator: 'moeru-ai',
   character_version: '0.1',
   ...data,
-}, merge)
+}, defu)
 
-export const overrideCharacterCard = (overrides: Partial<CharacterCardData>, defaults: CharacterCard) => defineCharacterCard(merge(overrides, defaults.data))
+export const overrideCharacterCard = (overrides: Partial<CharacterCardData>, defaults: CharacterCard) => defineCharacterCard(defu(overrides, defaults.data))
