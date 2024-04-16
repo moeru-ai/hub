b802b4e44a539a3d2811357a865592cea474d9a3
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-04-16 11:31:28 +0800

    fix(character-card-helpers/plist): add semicolon

--- packages/character-card-helpers/src/plist.ts
+++ packages/character-card-helpers/src/plist.ts
@@ -7,7 +7,7 @@ export type ListSeparator = ', ' | '/' | ({} & string)
  * {@link https://wikia.schneedc.com/bot-creation/trappu/introduction#plists}
  */
 export const lists = (...lists: string[]) =>
-  `[${lists.join('; ')}]`
+  `[${lists.join('; ')};]`
 
 export const list = (name: string, values: string | string[], separator: ListSeparator = ', ') =>
   `${name}: ${Array.isArray(values) ? values.join(separator) : values}`
--- packages/character-card-helpers/test/plist.test.ts
+++ packages/character-card-helpers/test/plist.test.ts
@@ -52,6 +52,6 @@ describe('@moeru-ai/character-card-helper/alichat', () => {
       ])
     )
 
-    expect(result).toBe(`[Eden's appearance: hair(long, fiery), eyes(golden), alluring, dress(crismon, silk), sleeves(Juliet-style), bodice(tight), gold trim, skirts(pleated, full), tights(black, elegant), gloves(black), hearpin/earring(golden); Tags: fantasy, slice of life, romance; Scenario: Conversation bewtten {{user}} and {{char}}; Eden's persona: calm, soft-spoken, generous, velvety-toned, brillant, confident, extravagant, rich, brightest star, loves(singing, performing, giving out precious items, wine, being drunk), wants to be admired, elegant, poetic, young, plays the harp]`)
+    expect(result).toBe(`[Eden's appearance: hair(long, fiery), eyes(golden), alluring, dress(crismon, silk), sleeves(Juliet-style), bodice(tight), gold trim, skirts(pleated, full), tights(black, elegant), gloves(black), hearpin/earring(golden); Tags: fantasy, slice of life, romance; Scenario: Conversation bewtten {{user}} and {{char}}; Eden's persona: calm, soft-spoken, generous, velvety-toned, brillant, confident, extravagant, rich, brightest star, loves(singing, performing, giving out precious items, wine, being drunk), wants to be admired, elegant, poetic, young, plays the harp;]`)
   })
 })
