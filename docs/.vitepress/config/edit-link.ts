a933182aadfa590568dc524a9365113afa431b78
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-04-18 21:11:03 +0800

    chore(docs): update edit link config

--- docs/.vitepress/config/edit-link.ts
+++ docs/.vitepress/config/edit-link.ts
@@ -3,8 +3,11 @@ import type { DefaultTheme as Theme } from 'vitepress'
 export const editLink = {
     // pattern: 'https://github.com/moeru-ai/hub/edit/main/:path'
     pattern: ({ filePath }) => {
-        if (filePath.startsWith('characters/'))
-            return `https://github.com/moeru-ai/hub/edit/main/packages/characters/src/${filePath.slice(11, -3)}.ts`
-        else return `https://github.com/moeru-ai/hub/edit/main/docs/${filePath}`
+        const base = new URL('https://github.com/moeru-ai/hub/edit/main/')
+
+        if (filePath.startsWith('characters/') && !(filePath === 'characters/index.md'))
+            return new URL(`packages/characters/src/${filePath.slice(11, -3)}.ts`, base).href
+
+        return new URL(`docs/${filePath}`, base).href
     }
 } satisfies Theme.EditLink
