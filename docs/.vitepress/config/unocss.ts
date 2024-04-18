22efee008da1b5037c31babd78a2929d2e146388
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-04-18 18:26:35 +0800

    chore(docs/unocss): disable preset icons

--- docs/.vitepress/config/unocss.ts
+++ docs/.vitepress/config/unocss.ts
@@ -1,4 +1,9 @@
-import { defineConfig, presetAttributify, presetIcons, presetUno } from 'unocss'
+import {
+  defineConfig,
+  presetAttributify,
+  // presetIcons,
+  presetUno
+} from 'unocss'
 
 /** {@link https://github.com/nolebase/integrations/blob/main/uno.config.ts} */
 export default defineConfig({
@@ -8,16 +13,16 @@ export default defineConfig({
       dark: 'class',
     }),
     presetAttributify(),
-    presetIcons({
-      cdn: 'https://esm.sh/',
-      prefix: 'i-',
-      scale: 1.2, // size: 1.2 rem
-      extraProperties: {
-        'display': 'inline-block',
-        'vertical-align': 'middle',
-        'min-width': '1.2rem',
-      },
-      warn: true,
-    }),
+    // presetIcons({
+    //   cdn: 'https://esm.sh/',
+    //   prefix: 'i-',
+    //   scale: 1.2, // size: 1.2 rem
+    //   extraProperties: {
+    //     'display': 'inline-block',
+    //     'vertical-align': 'middle',
+    //     'min-width': '1.2rem',
+    //   },
+    //   warn: true,
+    // }),
   ],
 })
