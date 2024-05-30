127c1ea9f4275e80beb3b6581f74befa29574a56
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-05-30 22:44:05 +0800

    Revert "fix(characters/murasame): remove chat_1_3"
    This reverts commit 2bbb11b9fc14ff36c49f74d4b95d17d489558c81.

--- packages/characters/src/visual_novel/murasame.ts
+++ packages/characters/src/visual_novel/murasame.ts
@@ -2,7 +2,7 @@ import { START } from '@moeru-ai/character-card-helpers'
 import { chats } from '@moeru-ai/character-card-helpers/alichat.js'
 import { group, list, lists } from '@moeru-ai/character-card-helpers/plist.js'
 
-import { chat_1_1, chat_1_2 } from './murasame_description'
+import { chat_1_1, chat_1_2, chat_1_3 } from './murasame_description'
 import { scenario_2_3 } from './murasame_first_mes'
 
 import { defineCharacterCard, overrideCharacterCard } from '../utils'
@@ -66,7 +66,7 @@ export const murasame = defineCharacterCard({
     list(`${name}'s personality`, plist.personality()),
     list(`${name}'s role`, plist.role())
   ),
-  description: [chat_1_1, chat_1_2].join(`\n${START}\n`),
+  description: [chat_1_1, chat_1_2, chat_1_3].join(`\n${START}\n`),
   first_mes: scenario_2_3,
   character_version: '0.1',
 })
