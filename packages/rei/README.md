aaecf562689cf33707ae8d548e52b07833a25c7d
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-08-09 22:42:15 +0800

    feat: new package `rei`

---
+++ packages/rei/README.md
@@ -0,0 +1,11 @@
+# REI「零」
+
+> Decentralized LLM Character Preset REI
+
+## Usage
+
+> TODO
+
+## License
+
+[MIT](../../LICENSE-MIT)
---
+++ packages/rei/src/factory.ts
@@ -0,0 +1,21 @@
+import { defineCharacterCard } from '@moeru-ai/character-card-helpers'
+import { defu } from 'defu'
+
+import pkg from '../package.json' with { type: 'json' }
+
+export interface DefineReiOptions {
+  name?: string
+}
+
+const defaults: DefineReiOptions = {
+  name: 'REI'
+}
+
+export const defineRei = (userOptions?: DefineReiOptions) => {
+  const { name } = defu(userOptions, defaults)
+
+  return defineCharacterCard({
+    name,
+    character_version: pkg.version
+  })
+}
