0bba8431b9c4882ea0ec08297743ee511acf6a02
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-11-01 22:13:14 +0800

    refactor(character-card-builder): update path

--- packages/character-card-builder/src/types/index.ts
+++ packages/character-card-builder/src/types/index.ts
@@ -1,12 +1,5 @@
-import type { Data } from './data'
-
-export interface CharacterCard {
-  data: Data
-  spec: 'chara_card_v3'
-  spec_version: '3.0'
-}
-
 export type { Asset, Assets } from './assets'
 export type { CharacterBook, CharacterBookEntry } from './character_book'
+export type { CharacterCard } from './character_card_v3'
 export type { Data, DataV2, DataV3 } from './data'
 export type { Extensions } from './extensions'
