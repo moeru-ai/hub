a99b154c70319defe9e4c919d5603557022206e7
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-11-02 19:51:13 +0800

    feat(character-card-builder): paragraph util

--- packages/character-card-builder/src/utils/markdown.ts
+++ packages/character-card-builder/src/utils/markdown.ts
@@ -1,3 +1,5 @@
 export const content = (...contents: string[]) => contents.join('\n\n')
 
 export const h = (length: 1 | 2 | 3 | 4 | 5 | 6, str: string) => `${Array.from({ length }).fill('#').join('')} ${str}`
+
+export const p = String.raw
