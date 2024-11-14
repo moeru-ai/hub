e79460e1d9e092ecd6a5facd833d415f8f2618dc
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-11-14 22:53:53 +0800

    refactor(ccc): improve types

--- packages/ccc/src/define/card.ts
+++ packages/ccc/src/define/card.ts
@@ -1,26 +1,66 @@
 import type { Data } from '../export/types'
 
-/**
- * Moeru-AI Character Card Markdown
- */
-export interface Card {
-  /**
-   * @experimental
-   * TODO: remove this
-   */
-  alternate_greetings: string[]
-  /**
-   * @experimental
-   * TODO: remove this
-   */
-  first_mes: string
+interface CardCore {
+  creator?: Data['creator']
   name: Data['name']
   /**
-   * @experimental
-   * TODO: remove this
+   * Nickname
+   * @see {@link https://github.com/kwaroran/character-card-spec-v3/blob/main/SPEC_V3.md#nickname}
    */
-  rawDescription: string
+  nickname?: Data['nickname']
   version: Data['character_version']
 }
 
+interface CardMeta {
+  /**
+   * Metadata.
+   *
+   * @example
+   * ```ts
+   * {
+   *   metadata: {
+   *     avatar: 'https://example.com/avatar.png',
+   *     foo: 721,
+   *     moetalk: true,
+   *   }
+   * }
+   * ```
+   */
+  metadata: Record<string, boolean | number | string>
+}
+
+interface CardAdditional {
+  /**
+   * First message and alternate greetings.
+   *
+   * `greetings[0]` - first_mes
+   *
+   * `greetings.slice(1)` - alternate_greetings
+   */
+  greetings: string[]
+  /**
+   * creator_notes
+   * @see {@link https://github.com/kwaroran/character-card-spec-v3/blob/main/SPEC_V3.md#creator_notes}
+   */
+  notes?: Data['creator_notes']
+  /**
+   * creator_notes_multilingual
+   * @see {@link https://github.com/kwaroran/character-card-spec-v3/blob/main/SPEC_V3.md#creator_notes_multilingual}
+   */
+  notes_multilingual?: Data['creator_notes_multilingual']
+}
+
+interface CardDescription {
+
+}
+
+/**
+ * Moeru-AI Character Card
+ */
+export type Card = CardAdditional & CardCore & CardDescription & CardMeta
+
+export type CardFn<T extends Record<string, unknown> = Record<string, unknown>> = (data: T) => Card
+
 export const defineCard = (card: Card) => card
+
+export const defineCardFn = <T extends Record<string, unknown> = Record<string, unknown>>(card: CardFn<T>, data: T) => card(data)
--- packages/ccc/src/export/types/character_card_v3.ts
+++ packages/ccc/src/export/types/character_card_v3.ts
@@ -1,6 +1,6 @@
 import type { Data } from './data'
 
-export interface CharacterCard {
+export interface CharacterCardV3 {
   data: Data
   spec: 'chara_card_v3'
   spec_version: '3.0'
