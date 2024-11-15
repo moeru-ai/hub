136f1d4e731a636da129d1a0d9d002d37f44198a
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-11-15 22:20:09 +0800

    fix(ccc): update types export

--- packages/ccc/src/export/types/index.ts
+++ packages/ccc/src/export/types/index.ts
@@ -1,5 +1,5 @@
 export type { Asset, Assets } from './assets'
 export type { CharacterBook, CharacterBookEntry } from './character_book'
-export type { CharacterCard } from './character_card_v3'
+export type { CharacterCardV3 } from './character_card_v3'
 export type { Data, DataV2, DataV3 } from './data'
 export type { Extensions } from './extensions'
