7571e2a3f27133554b8c7dd9c1c5a1ee9643d925
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-11-15 22:38:57 +0800

    refactor(ccc/export): more alias

--- packages/ccc/src/export/index.ts
+++ packages/ccc/src/export/index.ts
@@ -1,4 +1,4 @@
-export { exportToAPNG } from './apng'
-export { exportToJSON } from './json'
+export { exportToAPNG, exportToAPNG as exportToApng } from './apng'
+export { exportToJSON, exportToJSON as exportToJson } from './json'
 export { exportToMD, exportToMD as exportToMarkdown } from './md'
-export { exportToPNG } from './png'
+export { exportToPNG, exportToPNG as exportToPng } from './png'
