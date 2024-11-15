e36f2cf9857596ac9c7cf402a8d297809af7cca6
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-11-15 22:42:56 +0800

    refactor(ccc/utils/chat): add comment

--- packages/ccc/src/utils/chat.ts
+++ packages/ccc/src/utils/chat.ts
@@ -8,8 +8,28 @@ const prefixAndSuffix = (prefixAndSuffix: string) =>
           : str
     }${prefixAndSuffix}`
 
+/**
+ * Generate action string.
+ *
+ * @example
+ * ```ts
+ * const foo = action`Hello, World` // `*Hello, World*`
+ * const bar = action('What is love?') // '*What is love?*'
+ * const baz = action(['lorem,', 'ipsum']) // '*lorem, ipsum*'
+ * ```
+ */
 export const action = prefixAndSuffix('*')
+
+/**
+ * Generate message string.
+ *
+ * @example
+ * ```ts
+ * const foo = message`Hello, World` // `"Hello, World"`
+ * const bar = message('What is love?') // '"What is love?"'
+ * const baz = message(['lorem,', 'ipsum']) // '"lorem, ipsum"'
+ * ```
+ */
 export const message = prefixAndSuffix('"')
 
-export { action as act }
-export { message as msg }
+export { action as act, message as msg }
