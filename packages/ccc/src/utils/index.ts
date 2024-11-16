dc43074103524793e4590f00c3e1189f44c9d4bd
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-11-16 19:27:34 +0800

    feat(ccc): markdown utils

--- packages/ccc/src/utils/index.ts
+++ packages/ccc/src/utils/index.ts
@@ -1,3 +1,4 @@
 import * as chat from './chat'
+import * as markdown from './markdown'
 
-export { chat, chat as c }
+export { chat, chat as c, markdown, markdown as md }
---
+++ packages/ccc/src/utils/markdown.ts
@@ -0,0 +1,5 @@
+export const content = (...contents: string[]) => contents.join('\n\n')
+
+export const h = (length: 1 | 2 | 3 | 4 | 5 | 6, str: string) => `${Array.from({ length }).fill('#').join('')} ${str}`
+
+export const p = (arr: string[], separator = ' ') => arr.join(separator)
