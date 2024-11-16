c24aaf6afd1bf018016aecbbcb56ed106b6902f4
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-11-16 20:12:13 +0800

    refactor(ccc/export/types): split data

--- packages/ccc/src/export/types/data.ts
+++ packages/ccc/src/export/types/data.ts
@@ -3,28 +3,32 @@ import type { CharacterBook } from './character_book'
 import type { Extensions } from './extensions'
 
 /** @see {@link https://github.com/kwaroran/character-card-spec-v3/blob/main/SPEC_V3.md#charactercard-object} */
-export type Data = DataV2 & DataV3
+export type Data = DataV1 & DataV2 & DataV3
 
-/** fields & changes from CCV2 */
+/** @see {@link https://github.com/malfoyslastname/character-card-spec-v2/blob/main/spec_v1.md} */
+export interface DataV1 {
+  description: string
+  first_mes: string
+  mes_example: string
+  name: string
+  personality: string
+  scenario: string
+}
+
+/** @see {@link https://github.com/malfoyslastname/character-card-spec-v2/blob/main/spec_v2.md} */
 export interface DataV2 {
   alternate_greetings: string[]
   character_book?: CharacterBook
   character_version: string
   creator: string
   creator_notes: string
-  description: string
   extensions: Extensions
-  first_mes: string
-  mes_example: string
-  name: string
-  personality: string
   post_history_instructions: string
-  scenario: string
   system_prompt: string
   tags: string[]
 }
 
-/** new fields in CCV3 */
+/** @see {@link https://github.com/kwaroran/character-card-spec-v3/blob/main/SPEC_V3.md#charactercard-object} */
 export interface DataV3 {
   assets?: Assets
   creation_date?: number
