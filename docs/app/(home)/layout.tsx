b69395ded18abf32403c8c6c8f6afcb5208a5a1e
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-11-18 11:05:56 +0800

    chore: remove fumadocs

--- docs/app/(home)/layout.tsx
+++
@@ -1,6 +0,0 @@
-import type { PropsWithChildren } from 'react'
-
-import { baseOptions } from '@/app/layout.config'
-import { HomeLayout } from 'fumadocs-ui/layouts/home'
-
-export default ({ children }: PropsWithChildren) => <HomeLayout {...baseOptions}>{children}</HomeLayout>
