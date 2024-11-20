a348ac7c3b4ec048b660d4f71cec2f78ca8322bc
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-11-20 16:03:27 +0800

    chore: add eslint-plugin-astro

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
 import antfu from '@antfu/eslint-config'
 import ii from '@importantimport/eslint-config'
 
-export default antfu({ vue: true })
+export default antfu({ astro: true })
   .append(ii({ functional: false }))
