dd0fe3dd3d629595673e1cef2b283241dc40209e
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-12-16 21:02:17 +0800

    feat(ccc): add markdown link util

--- packages/ccc/src/utils/markdown.ts
+++ packages/ccc/src/utils/markdown.ts
@@ -3,3 +3,5 @@ export const content = (...contents: string[]) => contents.join('\n\n')
 export const h = (length: 1 | 2 | 3 | 4 | 5 | 6, str: string) => `${Array.from({ length }).fill('#').join('')} ${str}`
 
 export const p = (arr: string[], separator = ' ') => arr.join(separator)
+
+export const link = (content: string, href: string) => `[${content}](${href})`
