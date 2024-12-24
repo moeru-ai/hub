0b930f21722b0486667899fd9175a640ad5dc443
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-12-24 22:37:03 +0800

    chore(tsukuyomi_chan): update comments

--- packages/characters-existing/src/misc/tsukuyomi_chan/index.ts
+++ packages/characters-existing/src/misc/tsukuyomi_chan/index.ts
@@ -15,16 +15,13 @@ export const tsukuyomi_chan = defineCard({
     'license.link': 'https://tyc.rei-yumesaki.net/about/terms/',
     'license.name': 'Tsukuyomi-chan Character License',
   },
-  //   messageExample: [],
+  // TODO: use https://tyc.rei-yumesaki.net/material/kaiwa-ai
+  // messageExample: [],
   name: 'Tsukuyomi-chan',
   notes: [
     'These are unofficial character card of "つくよみちゃん" (Tsukuyomi-chan), the Free Material Character from Japan.',
     'Based on "Tsukuyomi-chan Character License".',
-    '■ Tsukuyomi-chan Official Site: https://tyc.rei-yumesaki.net/',
-
-    // 'The Character Card is created using a set of conversation text data that is made available free of charge by the free material character "Tsukuyomi-chan".',
-    // '■ つくよみちゃん会話AI育成計画: https://tyc.rei-yumesaki.net/material/kaiwa-ai/',
-    // '© Rei Yumesaki',
+    'Tsukuyomi-chan Official Site: https://tyc.rei-yumesaki.net/',
   ].join('\n\n'),
   version: '0.0',
 })
