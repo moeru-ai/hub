4927976493783c4aacd5516db6ead86777a97ef4
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-12-27 21:52:52 +0800

    refactor(ccc/export/png): named import

--- packages/ccc/src/export/png.ts
+++ packages/ccc/src/export/png.ts
@@ -1,12 +1,12 @@
-import PNG from 'meta-png'
+import { addMetadata, addMetadataFromBase64DataURI } from 'meta-png'
 
 import type { Card } from '../define'
 
 import { exportToJSON } from './json'
 
 export const exportToPNG = (data: Card, png: Uint8Array) =>
-  PNG.addMetadata(png, 'ccv3', btoa(JSON.stringify(exportToJSON(data))))
+  addMetadata(png, 'ccv3', btoa(JSON.stringify(exportToJSON(data))))
 
 export const exportToPNGBase64 = (data: Card, png: `data:image/png;base64,${string}`) =>
   // eslint-disable-next-line node/prefer-global/buffer
-  PNG.addMetadataFromBase64DataURI(png, 'ccv3', Buffer.from(JSON.stringify(exportToJSON(data))).toString('base64'))
+  addMetadataFromBase64DataURI(png, 'ccv3', Buffer.from(JSON.stringify(exportToJSON(data))).toString('base64'))
