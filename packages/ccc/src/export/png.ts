db6a84537bb860a5b751a05ad51fa8822ed7172f
藍+85CD <50108258+kwaa@users.noreply.github.com>
2024-12-27 22:16:10 +0800

    fix(ccc/export/png): do not use buffer

--- packages/ccc/src/export/png.ts
+++ packages/ccc/src/export/png.ts
@@ -5,8 +5,7 @@ import type { Card } from '../define'
 import { exportToJSON } from './json'
 
 export const exportToPNG = (data: Card, png: Uint8Array) =>
-  addMetadata(png, 'ccv3', btoa(JSON.stringify(exportToJSON(data))))
+  addMetadata(png, 'ccv3', btoa(String.fromCharCode(...new TextEncoder().encode(JSON.stringify(exportToJSON(data))))))
 
-export const exportToPNGBase64 = (data: Card, png: `data:image/png;base64,${string}`) =>
-  // eslint-disable-next-line node/prefer-global/buffer
-  addMetadataFromBase64DataURI(png, 'ccv3', Buffer.from(JSON.stringify(exportToJSON(data))).toString('base64'))
+export const exportToPNGBase64 = (data: Card, png: string) =>
+  addMetadataFromBase64DataURI(png, 'ccv3', btoa(String.fromCharCode(...new TextEncoder().encode(JSON.stringify(exportToJSON(data))))))
